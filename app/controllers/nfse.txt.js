39911803df1af6cf3699383f6156cab5bf19c5cd
Cledemir Barduco Junior <barduco@gmail.com>
2019-12-04 19:14:35 -0300

    exemplo soap

--- app/controllers/nfse.txt.js
+++ app/controllers/nfse.txt.js
@@ -386,3 +386,28 @@ function MyKeyInfo() {
         return fs.readFileSync(process.env.CERTIFICADO)
     }
 }
+
+
+
+/*
+const soapRequest = require('easy-soap-request');
+const fs = require('fs');
+
+// example data
+const url = 'https://graphical.weather.gov/xml/SOAP_server/ndfdXMLserver.php';
+const sampleHeaders = {
+  'user-agent': 'sampleTest',
+  'Content-Type': 'text/xml;charset=UTF-8',
+  'soapAction': 'https://graphical.weather.gov/xml/DWMLgen/wsdl/ndfdXML.wsdl#LatLonListZipCode',
+};
+const xml = fs.readFileSync('test/zipCodeEnvelope.xml', 'utf-8');
+
+// usage of module
+(async () => {
+  const { response } = await soapRequest({ url: url, headers: sampleHeaders, xml: xml, timeout: 1000 }); // Optional timeout parameter(milliseconds)
+  const { headers, body, statusCode } = response;
+  console.log(headers);
+  console.log(body);
+  console.log(statusCode);
+})();
+*/
